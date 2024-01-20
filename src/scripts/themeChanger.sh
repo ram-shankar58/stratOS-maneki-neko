7db86eeddd1d7b941afc15c5b935e3b05beded3c
ZeStig <69384921+zstg@users.noreply.github.com>
2024-01-20 20:55:14 +0530

    Update themeChanger.sh

--- src/scripts/themeChanger.sh
+++ src/scripts/themeChanger.sh
@@ -1,5 +1,50 @@
 #!/usr/bin/env bash
+gsettings set org.gnome.desktop.interface cursor-theme "'Bibata-Modern-Classic'" # StratOS-repo package
+# For GTK4 apps, refer etc/skel/.config/gtk-4.0/
 
-echo "Enter Choice:" && read choice
-echo "Your choice is $choice"
-sleep 3
+#!/bin/bash
+
+# Display menu options
+menu() {
+    dialog --clear --backtitle "StratOS theme changer" \
+        --title "Select a theme" \
+        --menu "Choose one of the following options:" 15 40 3 \
+        1 "TokyoNight Storm (default)" \
+        2 "TokyoNight Dark" \
+        3 "Gruvbox" \
+        2>&1 >/dev/tty
+}
+
+# Main loop
+while true; do
+    # Show menu and capture user choice
+    choice=$(menu)
+
+    # Check the user's choice
+    case $choice in
+        1)
+	    # Add your logic for Option A here
+	    gsettings set org.gnome.shell.extensions.user-theme name "'Tokyonight-Dark-B'"
+	    gsettings set org.gnome.desktop.interface gtk-theme "'Tokyonight-Dark-B'"
+	    gsettings set org.gnome.desktop.interface icon-theme "'Tokyonight-Dark'" 
+	    ln -sf $HOME/.themes/Tokyonight-Dark-B/gtk-4.0/  ~/.config/
+            ;;
+	2)
+	    gsettings set org.gnome.shell.extensions.user-theme name "'Tokyonight-Storm-B'"
+	    gsettings set org.gnome.desktop.interface gtk-theme "'Tokyonight-Storm-B'"
+	    gsettings set org.gnome.desktop.interface icon-theme "'Tokyonight-Moon'" 
+	    ln -sf $HOME/.themes/Tokyonight-Storm-B/gtk-4.0/  ~/.config/
+            ;;
+        3)
+	    gsettings set org.gnome.shell.extensions.user-theme name "'Gruvbox-Dark-B'"
+	    gsettings set org.gnome.desktop.interface gtk-theme "'Gruvbox-Dark-B'"
+	    gsettings set org.gnome.desktop.interface icon-theme "'Gruvbox-Dark'" 
+	    ln -sf $HOME/.themes/Gruvbox-Dark-B/gtk-4.0/  ~/.config/
+            ;;
+        *)
+            # User pressed Cancel or Escape
+            echo "Exiting..."
+            exit 0
+            ;;
+    esac
+done
